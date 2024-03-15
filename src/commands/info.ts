9a3bcfb5d50c93aa5c162460f98021a6b19e2821
sa556693828 <sa556693828@gmail.com>
2024-03-15 23:50:04 +0800

    Update_getOrCreateChatUser

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -7,7 +7,7 @@ const debug = createDebug('bot:about_command');
 const info = () => async (ctx: Context) => {
   debug(`Triggered "info" command with message`);
   const data = await getArtistData();
-  console.log('data', data);
+
   await ctx.replyWithMarkdownV2('info', { parse_mode: 'Markdown' });
 };
 
