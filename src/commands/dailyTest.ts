6ad8bffa53946009352b5be2dc5d8012d080e28d
sa556693828 <sa556693828@gmail.com>
2024-03-20 15:27:52 +0800

    Supabase

--- src/commands/dailyTest.ts
+++ src/commands/dailyTest.ts
@@ -4,18 +4,20 @@ import createDebug from 'debug';
 const debug = createDebug('bot:about_command');
 
 const WEB_APP_URL = 'https://feathers.studio/telegraf/webapp/example';
+const testUrl =
+  'https://6ae4-2001-b011-13-b16f-bd41-992a-f5ec-8e2a.ngrok-free.app/';
 
 const dailyTest = () => async (ctx: Context) => {
   debug(`Triggered "dailyTest" command with message`);
   // await ctx.setChatMenuButton({
   //   text: 'Launch',
   //   type: 'web_app',
-  //   web_app: { url: WEB_APP_URL },
+  //   web_app: { url: testUrl },
   // });
   const message = `*Go To Your Daily Test \\!*`;
   await ctx.replyWithMarkdownV2(
     message,
-    Markup.inlineKeyboard([Markup.button.webApp('📖', WEB_APP_URL)]),
+    Markup.inlineKeyboard([Markup.button.webApp('📖', testUrl)]),
   );
 };
 
--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,69 +1,44 @@
-import { getOrCreateUser, inviteFromUser } from '@/supabase';
+import { getOrCreateUser } from '@/supabase';
 import createDebug from 'debug';
-import { fetchinviter } from '@/supabase';
-import { tableMap } from '@/types';
+import { fetchInviter } from '@/supabase';
 
 const debug = createDebug('bot:about_command');
 
 const start = () => async (ctx: any) => {
   debug(`Triggered "start" command with message`);
-  const profileUrl = 'https://tgt-dashboard.vercel.app/profile/';
-
+  const profileUrl = process.env.TMA_URL;
   const keyboardMarkup = {
-    inline_keyboard: [[{ text: '你老媽', url: profileUrl }]],
+    inline_keyboard: [[{ text: 'url', url: profileUrl }]],
   };
   const userName = ctx.message?.from.username;
   const firstName = ctx.message?.from.first_name;
-  //console.log(ctx);
   const inviteFrom = ctx.payload;
   const userId = ctx.from.id;
-  const data = await fetchinviter(tableMap.users, userId);
-  const inviter = data[0].inviteFrom_id;
-  console.log('1. inviter: ', inviter);
-  console.log('2. inviteFrom: ', inviteFrom);
-  console.log('3. userId: ', userId);
-  //console.log('your inviter : ' + inviter);
-  //console.log(ctx.message);
-  //console.log('--------my name is:', userId,'-----------');
+  const data = await fetchInviter(userId);
+  const inviter = data && data[0]?.inviteFrom_id;
 
   const user = await getOrCreateUser(
     userId as number,
     userName ? userName : firstName ? firstName : '',
   );
-  /*if (inviteFrom && user.length > 0) {
-    //TODO: 防呆，如果已經有邀請者了，就不要再更新了。userId不能等於inviteFromId 1.自己不能邀請自己 2. 沒有邀請者 3. 邀請者已存在
-    await inviteFromUser(userId, inviteFrom);
-  }*/
 
   if (inviteFrom === '') {
-    //console.log('you are not invited from anyone');
-    await ctx.replyWithMarkdownV2('You are not invited from anyone', {
+    await ctx.replyWithMarkdownV2('Welcome! You are not invited from anyone', {
       // reply_markup: keyboardMarkup,
       parse_mode: 'Markdown',
     });
   } else if (inviteFrom === userId) {
-    //console.log('you are invited from yourself');
-    await ctx.replyWithMarkdownV2('You are invited from yourself', {
+    await ctx.replyWithMarkdownV2('You connot invite yourself', {
       // reply_markup: keyboardMarkup,
       parse_mode: 'Markdown',
     });
   } else {
-    //const inviteString = `@${inviter}`;
-    //console.log('you are invited from '+ inviteString);
     const value = inviteFrom == inviter ? inviteFrom : inviter;
     await ctx.replyWithMarkdownV2('You are invited from ' + value, {
       //reply_markup: keyboardMarkup,
       parse_mode: 'Markdown',
     });
   }
-  // else{
-  //   console.log("false");
-  // }
-
-  /*await ctx.replyWithMarkdownV2('test start', {
-    reply_markup: keyboardMarkup,
-    parse_mode: 'Markdown',
-  });*/
 };
 
 export { start };
