53ffa7d8e03d6099a88f8ac42a71e7dbb8dade6e
sa556693828 <sa556693828@gmail.com>
2024-03-25 06:52:38 +0800

    new

--- src/commands/dailyTest.ts
+++ src/commands/dailyTest.ts
@@ -3,22 +3,24 @@ import createDebug from 'debug';
 
 const debug = createDebug('bot:about_command');
 
-const WEB_APP_URL = 'https://feathers.studio/telegraf/webapp/example';
+const example = 'https://feathers.studio/telegraf/webapp/example';
 const testUrl =
-  'https://1480-2001-b011-13-b16f-bd41-992a-f5ec-8e2a.ngrok-free.app/';
+  'https://24ba-2001-b011-13-7cf7-4543-337b-ad2d-97ba.ngrok-free.app';
+const prodUrl = process.env.TMA_URL as string;
+const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const dailytest = () => async (ctx: Context) => {
   debug(`Triggered "dailytest" command with message`);
+  const url = ENVIRONMENT !== 'production' ? testUrl : prodUrl;
   // await ctx.setChatMenuButton({
   //   text: 'Launch',
   //   type: 'web_app',
   //   web_app: { url: testUrl },
   // });
-  const message = `*Go To Your Daily Test \\!*
-  Click on the button below to launch your daily test\\!`;
+  const message = `*Go To Your Daily Test \\!*\nClick on the button below to launch your daily test\\!\n${ENVIRONMENT !== 'production' ? 'This is a test environment' : ''}`;
   await ctx.replyWithMarkdownV2(
     message,
-    Markup.inlineKeyboard([Markup.button.webApp('📖', testUrl)]),
+    Markup.inlineKeyboard([Markup.button.webApp('📖', url)]),
   );
 };
 
