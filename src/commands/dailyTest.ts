ac21eee16fc532979c47faf62caf86ff718b41b3
sa556693828 <sa556693828@gmail.com>
2024-04-15 16:56:58 +0900

    Update URL

--- src/commands/dailyTest.ts
+++ src/commands/dailyTest.ts
@@ -3,14 +3,13 @@ import createDebug from 'debug';
 
 const debug = createDebug('bot:about_command');
 
-const example = 'https://feathers.studio/telegraf/webapp/example';
-const testUrl = 'https://e91f-39-128-55-53.ngrok-free.app/';
+const testUrl = 'https://503c-118-103-63-140.ngrok-free.app';
 const prodUrl = process.env.TMA_URL as string;
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const dailyTest = () => async (ctx: Context) => {
   debug(`Triggered "dailytest" command with message`);
-  const url = ENVIRONMENT !== 'production' ? testUrl : testUrl;
+  const url = ENVIRONMENT !== 'production' ? prodUrl : testUrl;
   // await ctx.setChatMenuButton({
   //   text: 'Launch',
   //   type: 'web_app',
